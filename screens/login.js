1a14be110ba4d1edde0385fcd0eeed9d55bf66d2
mgd888 <68098590+mgd888@users.noreply.github.com>
2020-10-24 00:40:05 -0600

    Update login.js

--- screens/login.js
+++ screens/login.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import * as React from 'react';
+import {  StyleSheet, Text, TextInput, View, Button, TouchableOpacity } from 'react-native';
+
 
 export default function Login({navigation}) {
 
@@ -9,19 +10,29 @@ export default function Login({navigation}) {
 
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor:"grey" }}>
-      <Text style={{
-        alignSelf:'center',
-        fontSize: 45,
-        color: 'white',
-        paddingBottom: 10,
-        marginBottom:20,
-        borderBottomColor: 'grey',
-        borderBottomWidth: 1,
-        fontWeight:'bold',
-        paddingLeft:60,
-        paddingRight:60
-        }}>Log In</Text>
-      <TextInput style={{
+        <Text style={{
+          alignSelf:'center',
+          fontSize: 45,
+          color: 'white',
+          paddingBottom: 10,
+          marginBottom:20,
+          borderBottomColor: 'grey',
+          borderBottomWidth: 1,
+          fontWeight:'bold',
+          paddingLeft:60,
+          paddingRight:60
+          }}>Log In</Text>
+        <TextInput style={{
+            alignSelf:'center',
+            height: 20,
+            marginBottom: 20,
+            color:'white',
+            borderBottomColor: 'white',
+            borderBottomWidth: 1,
+            paddingLeft:60,
+            paddingRight:60
+        }} placeholder="Username"></TextInput>
+        <TextInput style={{
           alignSelf:'center',
           height: 20,
           marginBottom: 20,
@@ -30,26 +41,15 @@ export default function Login({navigation}) {
           borderBottomWidth: 1,
           paddingLeft:60,
           paddingRight:60
-      }} placeholder="Username"></TextInput>
-      <TextInput style={{
-        alignSelf:'center',
-        height: 20,
-        marginBottom: 20,
-        color:'white',
-        borderBottomColor: 'white',
-        borderBottomWidth: 1,
-        paddingLeft:60,
-        paddingRight:60
-        
-      }} placeholder="Password" secureTextEntry={true}></TextInput>
-        {/* We will add where this button redirects later */}
-     {/* <Button title= "Log in" style={{alignSelf:'center', marginBottom: 20}} onPress={() => navigation.navigate('Dashboard')}/> */}
-      <Text style={{alignSelf:'center', marginBottom: 20, marginTop: 20}}>Don't have an account?</Text>
-      <Button
-        title="Sign Up Now"
-        onPress={pressHandler}
-      />
-    </View>
+  
+        }} placeholder="Password" secureTextEntry={true}></TextInput>
+        <Button title= "Log in" style={{alignSelf:'center', marginBottom: 20}} onPress={() => navigation.navigate('Signup')}/>
+        <Text style={{alignSelf:'center', marginBottom: 20, marginTop: 20}}>Don't have an account?</Text>
+        <Button
+          title="Sign Up Now"
+          onPress={pressHandler}
+        />
+      </View>
     );
 }
 
