16fa7eda5f348ff271d081f00353a87843b4305d
Jason Wolfe <jason.wolfe71@gmail.com>
2020-10-24 20:27:19 -0600

    Preliminary work on ratings screen & updated gitignore
    updated gitignore to ignore the .vs directory

--- screens/rating.js
+++ screens/rating.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { withOrientation } from 'react-navigation';
 
 export default function Rating({navigation}) {
 
@@ -9,7 +10,12 @@ export default function Rating({navigation}) {
 
     return (
         <View style={styles.container}>
-            <Text>Rating Screen</Text>
+            <Text>Submit Rating for *Region*</Text>
+            <Text>Rating:</Text>
+            <TextInput style={styles.commentArea}/>
+            <Text>Comment:</Text>
+            <TextInput style={styles.commentArea}/>
+            <Button title='Submit Rating'/>
             <Button title='Back to home' onPress={pressHandler} />
         </View>
     );
@@ -18,5 +24,9 @@ export default function Rating({navigation}) {
 const styles = StyleSheet.create({
     container: {
         padding: 24,
+    },
+    commentArea: {
+        backgroundColor: 'white'
+
     }
 });
