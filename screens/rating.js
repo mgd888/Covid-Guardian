13be8e85746457c712efc18ed60a6c3764026b5b
Jason Wolfe <jason.wolfe71@gmail.com>
2020-10-27 23:28:52 -0600

    Add dynamic title based on region id
    we could move the function to a separate file

--- screens/rating.js
+++ screens/rating.js
@@ -18,8 +18,60 @@ export default function Rating(props) {
 
     const [commentData, setCommentData] = useState(''); //add commentData to state
     const [reviewData, setReviewData] = useState(-1); //add reviewData to state
+    const [title, setTitle] = useState('title');
 
     const regionID = props.navigation.getParam('regionID', -1); //get the regionID from props. default to -1 if not passed
+
+    //TODO: move this into a 'global library' since this will be used throughout the app
+    const getRegionString = (regionID) => {
+        let result;
+        switch (regionID) {
+            case 0:
+                result = "Far North West";
+                break;
+            case 1:
+                result = "Far North Central";
+                break;
+            case 2:
+                result = "Far North East";
+                break;
+            case 3:
+                result = "North West";
+                break;
+            case 4:
+                result = "North Central";
+                break;
+            case 5:
+                result = "North East";
+                break;
+            case 6:
+                result = "Saskatoon";
+                break;
+            case 7:
+                result = "Central West";
+                break;
+            case 8:
+                result = "Central East";
+                break;
+            case 9:
+                result = "Regina";
+                break;
+            case 10:
+                result = "South West";
+                break;
+            case 11:
+                result = "South Central";
+                break;
+            case 12:
+                result = "South East";
+                break;
+            default:
+                result = "invalid region: " + regionID;
+                break;
+        }
+        return result;
+    }
+
 
     const pressHandler = () => {
         props.navigation.popToTop();
@@ -59,7 +111,7 @@ export default function Rating(props) {
 
             <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
                 <View style={styles.inner}>
-                    <Text style={styles.screenHeader}>Submit Rating for *Region*</Text>
+                    <Text style={styles.screenHeader}>{getRegionString(regionID)}</Text>
 
                     <Text style={styles.inputHeading}>Rating:</Text>
                     <View style={styles.textView}>
@@ -125,7 +177,7 @@ const styles = StyleSheet.create({
     },
     textView: {
         backgroundColor: 'white',
-        borderRadius: 25,
+        borderRadius: 5,
         marginTop: 5,
         marginBottom: 20,
         padding: 10
