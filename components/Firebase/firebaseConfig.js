4c5953ac3420021271c74906149fb003083b28fe
mgd888 <68098590+mgd888@users.noreply.github.com>
2020-11-04 17:51:33 -0600

    Maria (#12)
    * Update signup.js
    
    * Update login.js
    
    * Create firebaseConfig.js
    
    * Update login.js
    
    * Update signup.js
    
    Co-authored-by: Jason Wolfe <jason.wolfe71@gmail.com>

---
+++ components/Firebase/firebaseConfig.js
@@ -0,0 +1,23 @@
+/*
+firebaseConfig.js
+Author: Jason Wolfe
+
+Description:
+Holds the configuration for the firebase connections
+The values below are set to 'XXXX' by default. Upon
+cloning the reposityory you will have to adjust these to
+point to our firebase project.
+
+Copy and rename the file to: firebaseConfig.js
+
+*/
+
+export default {
+    apiKey: "AIzaSyBS-omBBCGNmeN28T6TQzIfzuqh2IJQbCM",
+    authDomain: "cs372-cta.firebaseapp.com",
+    databaseURL: "https://cs372-cta.firebaseio.com/",
+    projectId: "cs372-cta",
+    storageBucket: "cs372-cta.appspot.com",
+    messagingSenderId: "687658836000",
+    appId: "1:687658836000:web:8e3fc8270ca387576e9c45"
+  };
