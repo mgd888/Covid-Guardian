20ee84e70e710ff343d91451565f00af1dc4642d
lee200351644 <62478914+lee200351644@users.noreply.github.com>
2020-11-15 13:23:27 -0600

    Completed region
    With this the region data is fully functional and has complete CSS. the button is the same as the home screen to keep consistency.

--- screens/region.js
+++ screens/region.js
@@ -1,8 +1,5 @@
 import { 
-    Button, 
     FlatList,
-    Linking,
-    RefreshControl,
     StyleSheet, 
     Text,
     TouchableOpacity, 
@@ -20,6 +17,7 @@ const Emoji = props => (                               //reusable code for emoji
         role="img"
         aria-label={props.label ? props.label : ""}
         aria-hidden={props.label ? "false" : "true"}
+        style = {styles.ji}
     >
         {props.symbol}
     </Text>
@@ -28,6 +26,11 @@ const Emoji = props => (                               //reusable code for emoji
 
 export default function Region({navigation}) {
 
+
+    //packet to be sent to Comment screen when clicked
+    const [region] = useState([
+        { key: 1,  regionID: regionID},
+    ]);
 
     //set up and functions 
     let db = fb.fb.firestore();
@@ -50,9 +53,21 @@ export default function Region({navigation}) {
     let regionID =  navigation.getParam('regionID')
     let regionName = getRegionString(regionID);
 
+    //weighted data
+    let Drating;
+    //User percentage
+    let Upercentage;
+    //Government percentage
+    let Gpercentage;
+    //Emoji selector
+    let emo;
+    //Rating to be shown to Users
+    let rating;
 
-    if(Max == -1)
-    {
+
+    if(Max == -1)                                     //stops additional reads when state updates.
+    {                                                 //here to stop Freezing and lag issues
+
         //pull case file
         db.collection("cases").where("regionID", "==", regionID).orderBy("date", "desc").limit(1).get().then((snapshot) => {
                 snapshot.forEach((doc) => {
@@ -81,7 +96,7 @@ export default function Region({navigation}) {
                 let y = [];
                 snapshot.forEach((doc) => {
                 x = doc.get("rating") + x;
-                y.push(doc.data);
+                y.push({ userName:doc.data().userName, rating: doc.data().rating, comment: doc.data().comment, key: doc.id});
                 })
                 x = x/z;
                 setavg(x);
@@ -91,23 +106,19 @@ export default function Region({navigation}) {
     }
 
 
-
-
-    let Gpercentage = current/Max;       //Government percent       with current setup should be 9/12 = 0.75
-
-    if(avg == 0 || avg == NaN)
+    if(isNaN(avg) == true)               //if there are no ratings set avg to 3 as to have no impact
     {
-        avg = 3;
+        setavg(3);
     }
 
-    let Upercentage = 1-(avg/5);
+    Gpercentage = current/Max;       //Government percent
 
-    let Drating;  //weighted towards government
-    Drating = .6 * Gpercentage + .4 *Upercentage; //weighted 60/40
+    Upercentage = 1-(avg/5);        //invert it as high is bad in the mathematics
 
+    Drating = .6 * Gpercentage + .4 *Upercentage; //weighted 60/40 towards Government data
 
-    let emo;
-
+    //Based on the percentage of the approval rating we assign a specific emoji. it is broken down into 10s
+    //for the off chance that it is negative we have an error message so the rest of the data will show
     if(Drating >=.9)
     {
             emo = <Emoji symbol="🤮" label="everyhingSucks"/>
@@ -138,11 +149,11 @@ export default function Region({navigation}) {
     }
     else if(Drating <.3 && Drating >=.2)
     {
-            emo = <Emoji symbol="😐" label="bubbleHasn'tBurst"/>
+            emo = <Emoji symbol="😐" label="bubbleHasn'tBurst" />
     }
     else if(Drating <.2 && Drating >=.1)
     {
-            emo = <Emoji symbol="🙂" label="nearlyNormal"/>
+            emo = <Emoji symbol="🙂" label="nearlyNormal" />
     }
     else if(Drating <.1)
     {
@@ -153,35 +164,180 @@ export default function Region({navigation}) {
             emo = <Text>error</Text>
     }
 
+
+
+    rating = Math.floor((1- Drating)*100);
+
+
+    //needs to be here as data isn't found until now
+    //render Item for Flatlist to go through comments
+    const renderItem = ({item}) => (
+        <View style={styles.Concomm}>
+            <Text style={styles.user}>{item.userName}  Rating: {item.rating}</Text>
+            <Text>{item.comment}</Text>
+        </View>   
+    );
+
+
+    //what the User will see
     return(
-        <View st>
-            <Text style = {styles.title}>{regionName}</Text>
-            {emo}
-        </View>
+        <ScrollView>
+            <View style = {styles.container}>
+                <Text style = {styles.title}>{regionName}</Text>
+                <View style = {styles.row}>
+                    {emo} 
+                    <Text style = {styles.HRating}>Approval rating: {rating}%</Text>
+                </View>
+                <View style = {styles.stats}>
+                    <Text style = {styles.subTitle}>
+                        Statistics
+                    </Text>
+                    <Text>
+                        Current Cases: {current}
+                    </Text>
+
+                    <Text>
+                        New Cases: {newCases}
+                    </Text>
+
+                    <Text>
+                        ICU Cases: {icu}
+                    </Text>
+
+                    <Text>
+                        Hospitalized Cases: {inpatient}
+                    </Text>
+
+                    <Text>
+                        Recovered Cases: {recover}
+                    </Text>
+
+                    <Text>
+                        Total Cases: {total}
+                    </Text>
+
+                    <Text>
+                        Highest recorded Cases at one time: {Max}
+                    </Text>
+                </View>
+                <View>
+                    <TouchableOpacity onPress={() => navigation.navigate('Rating', region)}>
+                        <Text style={styles.button}>Add Rating.</Text>
+                    </TouchableOpacity>
+                </View>
+
+                <FlatList
+                data={item}
+                renderItem={renderItem}
+                />
+
+            </View>
+        </ScrollView>
     )
 }
 
 //bluebuttons for most thing default style otherwise
 const styles = StyleSheet.create({
-    container: {
+    container: {                         //Base format
         padding: 24,
         fontSize: 20,
         overflow: 'hidden',
         justifyContent: 'center',
         alignItems: 'center',
+        flex: 1,
+        flexDirection: "column",
+        justifyContent: "center",
     },
-    title: {
+    title: {                            //Title of page/region
+        fontSize: 30,
+        paddingLeft: 55,
+        paddingRight: 55,
+        paddingTop: 10,
+        paddingBottom: 10,
+        fontWeight: "bold",
+        width: 300,
+        borderRadius: 6,
+        textAlign: 'center',
+        backgroundColor: '#3DA5E0',
+    },
+    HRating:                           //Header approval rating
+    {
+        fontSize: 20,
+        width: 200,
+        height: 70,
+        paddingTop: 20,
+        borderRadius: 6,
+        textAlign: 'center',
+        backgroundColor: '#3DA5E0',
+    },
+
+    subTitle:                        //Sub header
+    {
+        fontSize: 22,
+        marginBottom: 10,
+        borderBottomWidth: 1, 
+        fontWeight: "bold",
+        textAlign: 'center',
+    },
+
+    ji:                             //emoji
+    {
+        fontSize: 50,
+        textAlign: 'left',
+    },
+
+    row:                           //align emoji and approval in a row format
+    {
+        fontSize: 30,
+        paddingLeft: 55,
+        paddingRight: 55,
+        paddingTop: 10,
+        paddingBottom: 10,
+        height:90,
+        flexDirection: "row",
+        justifyContent: "center",
+    },
+
+    stats:                       //Format for government statistics
+    {
+        fontSize: 20,
+        paddingTop: 20,
+        fontSize: 20,
+        padding: 10,
+        borderRadius: 6,
+        textAlign: 'center',
+        backgroundColor: '#3DA5E0',
+    },
+
+
+    button: {                  //Pressable button
         fontSize: 20,
         paddingLeft: 55,
         paddingRight: 55,
         paddingTop: 10,
         paddingBottom: 10,
+        marginTop: 10,
         marginBottom: 10,
         borderWidth: 1,
         borderRadius: 6,
-        width: 300,
-        textAlign: 'center',
-    }
+        backgroundColor: '#61dafb',
+        width: 280,
+        textAlign: 'left',
+    },
 
+
+    Concomm:                  //Comment format
+    {
+        marginBottom: 10,
+        borderWidth: 1,
+        padding: 10,
+        width: 280, 
+        textAlign: 'left',
+        backgroundColor: '#3DA5E0',
+    },
+
+    user: {                  //Username
+        fontWeight: "bold",
+    },
 
 });
